e8d8c5f8bdec28395697b46427a4b3633e0056c5
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-05 18:19:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,10 +17,7 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
-  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  small_dogs = dog_breeds.pop(2)
-  p dog_breeds
-  p small_dogs
+  array.pop(2)
 end
 
 def using_shift(array)
